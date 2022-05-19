0a35e975d6b4b416240b721ffb346eb491b9b29f
Yun-SeYeong <dbstpdud09@naver.com>
2022-05-19 11:35:49 +0900

    [ADD] test18108

---
+++ baekjoon-kotlin/src/test18108.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class test18108 {
+    companion object {
+        fun test18108() {
+            val br = BufferedReader(InputStreamReader(System.`in`))
+            print(Integer.parseInt(br.readLine()) - 543)
+        }
+    }
+}
